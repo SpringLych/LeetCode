5b5f0f338c909fdc28510bf334e9dff681a61337
SpringLych <2571568860@qq.com>
2018-03-17 17:49:12 +0800

    second commit

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,18 @@
+import java.util.HashMap;
+
 /**
  * @author LiYingChun
  * @date 2018/1/25
  */
 public class Test {
 
-    public static void fun(){
-        String a = "HelloWorld";
-        int[] b = new int[256];
-        char[] c = a.toCharArray();
-        for (char i : c){
-            int index = (int) i;
-            b[i] ++;
-        }
-        int index = 0;
-        int t;
-        for (int i : b){
-            System.out.print(index + " : " + i + ", ");
-            index ++;
-            t = index % 20;
-            if (t == 0){
-                System.out.println("\n");
-            }
-        }
-    }
+
     public static void main(String[] args){
-        fun();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(45,45);
+        //int result = map.get(47);
+        boolean isEsist = map.containsKey(47);
+        System.out.println("result = " + isEsist);
+
     }
 }
---
+++ src/easy/ContainsDuplicate.java
@@ -0,0 +1,49 @@
+package easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * @author LiYingChun
+ * @date 2018/3/17
+ * 217. Contains Duplicate
+ * Given an array of integers, find if the array contains any duplicates.
+ * Your function should return true if any value appears at least twice in the array,
+ * and it should return false if every element is distinct.
+ */
+public class ContainsDuplicate {
+    /**
+     * 使用HashM解法
+     * @param nums
+     * @return
+     */
+    public static boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int len = nums.length;
+        if (len == 0) return false;
+        boolean isDuplicate = false;
+        for (int i = 0; i < len; i++){
+            if (i ==0) continue;
+            map.put(nums[i - 1], nums[i - 1]);
+            if (map.containsKey(nums[i])){
+                isDuplicate = true;
+            }
+        }
+        return isDuplicate;
+    }
+
+    /**
+     * leetcode上更快的解法
+     * @param nums
+     * @return
+     */
+    public boolean containsDuplicate2(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == nums[i-1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
