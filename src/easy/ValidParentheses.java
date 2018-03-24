cb3fd05c7a0044c8fb773651b51927a62d4f8787
SpringLych <2571568860@qq.com>
2018-03-24 11:16:50 +0800

    6th commit

---
+++ src/easy/ValidParentheses.java
@@ -0,0 +1,35 @@
+package easy;
+
+import java.util.Stack;
+
+/**
+ * @author LiYingChun
+ * @date 2018/3/24
+ * 20. Valid Parentheses
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ */
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        char[] arr = s.toCharArray();
+        int len = arr.length;
+        boolean res = true;
+        for (char anArr : arr) {
+            if (anArr == '{' || anArr == '[' || anArr == '(') {
+                st.push(anArr);
+            }
+            if (anArr == '}' || anArr == ']' || anArr == ')') {
+                if (st.empty()) res = false;
+                else {
+                    boolean t = ((anArr == '}' && st.pop() == '{') ||
+                            (anArr == ']' && st.pop() == '[') ||
+                            (anArr == ')' && st.pop() == '('));
+                    if (!t) res = false;
+                }
+            }
+        }
+        if (!st.empty()) res = false;
+        return res;
+    }
+}
