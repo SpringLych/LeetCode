6a0f9aa86663b50e94b35c7199e71a40bec0c745
SpringLych <2571568860@qq.com>
2018-03-28 21:38:26 +0800

    8th commit

--- src/Test.java
+++ src/Test.java
@@ -8,11 +8,6 @@ public class Test {
 
 
     public static void main(String[] args){
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(45,45);
-        //int result = map.get(47);
-        boolean isEsist = map.containsKey(47);
-        System.out.println("result = " + isEsist);
 
     }
 }
