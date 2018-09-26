33fa52fbf7b3b0e30b6bf0d53df936bd059a1dce
SpringLych <2571568860@qq.com>
2018-09-26 22:01:51 +0800

    P88

---
+++ src/easy/P88_MergeSortedArray.java
@@ -0,0 +1,26 @@
+package easy;
+
+/**
+ * @author SpringLych
+ * @date 2018/9/26
+ * <p>
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ */
+public class P88_MergeSortedArray {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i];
+                i--;
+            } else {
+                nums1[k--] = nums2[j];
+                j--;
+            }
+        }
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
